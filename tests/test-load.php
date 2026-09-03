0f9bc7305c2ea067b461be77c33c378f3a6a4af0
selul <marius.cristea@vertistudio.com>
2026-09-03 22:05:46 +0300

    Render selected static homepage content

--- tests/test-load.php
+++ tests/test-load.php
@@ -31,4 +31,37 @@ class Test_Loading extends WP_UnitTestCase {
 	public function testCoreLoaded() {
 		$this->assertTrue( class_exists( 'NonProfitFSE\Core', false ) );
 	}
+
+	/**
+	 * The static front-page template must render the selected page's content.
+	 *
+	 * @return void
+	 */
+	public function testFrontPageRendersSelectedPageContent() {
+		$template = file_get_contents( get_template_directory() . '/templates/front-page.html' );
+		$blocks   = parse_blocks( $template );
+
+		$this->assertTrue( $this->containsBlock( $blocks, 'core/post-content' ) );
+	}
+
+	/**
+	 * Check a parsed block tree for a block name.
+	 *
+	 * @param array  $blocks     Parsed blocks.
+	 * @param string $block_name Block name to find.
+	 * @return bool
+	 */
+	private function containsBlock( $blocks, $block_name ) {
+		foreach ( $blocks as $block ) {
+			if ( isset( $block['blockName'] ) && $block_name === $block['blockName'] ) {
+				return true;
+			}
+
+			if ( ! empty( $block['innerBlocks'] ) && $this->containsBlock( $block['innerBlocks'], $block_name ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
